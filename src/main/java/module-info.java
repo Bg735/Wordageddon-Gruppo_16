cb4649562db5ec82c2e5fe49a5abb2632aac7e2e
Bg735 <f.lanzara04@gmail.com>
2025-05-13 13:17:52 +0200

    Start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.wordageddongruppo_16 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.wordageddongruppo_16 to javafx.fxml;
+    exports org.example.wordageddongruppo_16;
+}
