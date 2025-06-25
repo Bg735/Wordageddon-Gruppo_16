efb419a3d13aa97da7237d256fd1ee74832ccfbb
rossellapale <r.pale1@studenti.unisa.it>
2025-06-25 21:21:59 +0200

    Refactor lecture to lettura, update auth UI and controller

--- src/main/java/it/unisa/diem/wordageddon_g16/controllers/AuthController.java
+++ src/main/java/it/unisa/diem/wordageddon_g16/controllers/AuthController.java
@@ -1,9 +1,36 @@
 package it.unisa.diem.wordageddon_g16.controllers;
 
 import it.unisa.diem.wordageddon_g16.db.DAO;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AuthController {
+    @FXML
+    private Button RegistrazioneBtn;
+
+    @FXML
+    private Button accediBtn;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private TextField userTextField;
+
+    @FXML
+    void handleAccediBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRegistrazioneBtn(ActionEvent event) {
+
+    }
+
     private DAO<?> userDAO;
+
 
     public <T> AuthController(UserDAO userDAO) {
         this.userDAO = userDAO;
