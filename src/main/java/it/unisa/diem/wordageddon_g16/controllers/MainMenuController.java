d9b8278aecdb12b708e887304d87a73520192a62
rossellapale <r.pale1@studenti.unisa.it>
2025-06-25 13:43:38 +0200

    Add lecture view and update menu UI

--- src/main/java/it/unisa/diem/wordageddon_g16/controllers/MainMenuController.java
+++ src/main/java/it/unisa/diem/wordageddon_g16/controllers/MainMenuController.java
@@ -1,6 +1,13 @@
 package it.unisa.diem.wordageddon_g16.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 public class MainMenuController {
-    public <T> MainMenuController() {
+    public <T> MainMenuController() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/it/unisa/diem/wordaggedon_g16/fmxl/menu.fxml"));
+        Parent root = loader.load();
     }
 }
