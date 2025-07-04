37a03e862aabb427aba2221807539a346d45126d
emanueletocci <tocciemanuele03@gmail.com>
2025-07-04 17:51:47 +0200

    Revert "rimozione modulo"
    This reverts commit 4cf5dff08aff8f40ed3f67dadabddb96520de6cc.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module it.unisa.diem.wordageddongruppo_16 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens it.unisa.diem.wordageddon_g16 to javafx.fxml;
+    opens it.unisa.diem.wordageddon_g16.controllers to javafx.fxml;
+    exports it.unisa.diem.wordageddon_g16;
+}
