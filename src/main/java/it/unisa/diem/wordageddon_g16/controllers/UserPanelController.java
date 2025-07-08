b1f5b822464b656caf9b35185b3e2668cabd5e34
Emanuele Tocci <emato03.felix@gmail.com>
2025-07-08 12:16:32 +0200

    Revert "Merge branch 'main' into UserPanel"
    This reverts commit 1815747bbccf7bed8a7342cd8c35c3c0c753e553, reversing
    changes made to 15ff7947c31a1480d1b23bf2d276ab43536d9038.

--- src/main/java/it/unisa/diem/wordageddon_g16/controllers/UserPanelController.java
+++ src/main/java/it/unisa/diem/wordageddon_g16/controllers/UserPanelController.java
@@ -1,4 +1,6 @@
 package it.unisa.diem.wordageddon_g16.controllers;
+
+
 import it.unisa.diem.wordageddon_g16.models.AppContext;
 import it.unisa.diem.wordageddon_g16.models.Document;
 import it.unisa.diem.wordageddon_g16.models.GameReport;
